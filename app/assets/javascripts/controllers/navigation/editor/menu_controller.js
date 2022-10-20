b602516ba62d1f51c3013ea49feec1a101e1f928
Stephen Nelson <stephen@katalyst.com.au>
2022-10-20 19:44:35 +1030

    FRIN23-169: Fix dropping a top level item as the first child of a menu

--- app/assets/javascripts/controllers/navigation/editor/menu_controller.js
+++ app/assets/javascripts/controllers/navigation/editor/menu_controller.js
@@ -37,10 +37,13 @@ export default class MenuController extends Controller {
 
     let delta = 0;
     if (previous === undefined) {
+      // if previous does not exist, set depth to 0
       delta = -item.depth;
-    } else if (previous.hasExpandedDescendants()) {
+    } else if (item.nextItem && item.nextItem.depth > previous.depth) {
+      // if next is a child of previous, make item a child of previous
       delta = previous.depth - item.depth + 1;
     } else {
+      // otherwise, make item a sibling of previous
       delta = previous.depth - item.depth;
     }
 
