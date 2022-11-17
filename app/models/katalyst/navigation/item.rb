439c6d4d26c2cf4c5dd8c8d6800f6996ba2c06e9
Alan Cornthwaite <alan.cornthwaite@katalyst.com.au>
2022-11-17 15:11:07 +1030

    Add target options to item
    Merge http_method depending on target

--- app/models/katalyst/navigation/item.rb
+++ app/models/katalyst/navigation/item.rb
@@ -10,18 +10,38 @@ module Katalyst
 
       attr_accessor :parent, :children, :index, :depth
 
+      TARGETS = %i[self _top _blank kpop].index_by(&:itself).freeze
+
+      enum method: TARGETS, _prefix: :target
+
       def self.permitted_params
         %i[
           title
           url
           visible
-          new_tab
+          target
           type
         ]
       end
 
       def layout?
         is_a? Layout
+      end
+
+      # Entrypoint for combining additional options based on attribute rules
+      def item_options
+        options_for_target
+      end
+
+      def options_for_target
+        return {} if target == "self"
+
+        options = { target: target }
+
+        unless target == "_blank" || target == "_top"
+          options = { data: { turbo: true, turbo_frame: target } }
+        end
+        options
       end
 
       private
