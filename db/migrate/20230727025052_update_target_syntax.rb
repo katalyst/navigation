eb28047ea3e6832d36daf123031257a9ca7d5622
Stephen Nelson <stephen@katalyst.com.au>
2023-07-28 11:27:28 +0930

    Fix bad migration for http_method

--- db/migrate/20230727025052_update_target_syntax.rb
+++ db/migrate/20230727025052_update_target_syntax.rb
@@ -3,7 +3,7 @@
 # Ensures that all navigation items have a valid target and http_method.
 class UpdateTargetSyntax < ActiveRecord::Migration[7.0]
   def change
-    Katalyst::Navigation::Item.where(http_method: nil).update_all(target: "get")
+    Katalyst::Navigation::Item.where(http_method: nil).update_all(http_method: "get")
     Katalyst::Navigation::Item.where(target: nil).update_all(target: "self")
     Katalyst::Navigation::Item.where(target: "_blank").update_all(target: "blank")
     Katalyst::Navigation::Item.where(target: "_top").update_all(target: "top")
