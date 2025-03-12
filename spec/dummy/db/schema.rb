a37ad3b0fbc60fa197a621c452121f0acba24da4
Stephen Nelson <stephen@katalyst.com.au>
2025-03-12 21:51:04 +1030

    Churn: update dependencies

--- spec/dummy/db/schema.rb
+++ spec/dummy/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_07_27_025052) do
+ActiveRecord::Schema[8.0].define(version: 2023_07_27_025052) do
   create_table "katalyst_navigation_items", force: :cascade do |t|
     t.integer "menu_id", null: false
     t.string "type"
