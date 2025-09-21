8a9a8df3cbf1ab3c65cce2a0b6d1cac80bfd5386
Stephen Nelson <stephen@katalyst.com.au>
2025-09-22 09:23:26 +0930

    Block external font assets in tests

--- spec/support/capybara.rb
+++ spec/support/capybara.rb
@@ -1,19 +1,29 @@
 # frozen_string_literal: true
 
-require "capybara/rspec"
 require "capybara/cuprite"
+require "capybara/rspec"
 
 RSpec.configure do |config|
   Capybara.default_driver = Capybara.javascript_driver = :cuprite
 
-  Capybara.server                = :puma, { Silent: true }
-  Capybara.disable_animation     = true
+  Capybara.server            = :puma, { Silent: true }
+  Capybara.disable_animation = true
 
   # Rails will set `:selenium` as the runner for system tests by default, but this happens after `before` hooks.
   # We want to use our configured javascript driver and ensure that this is set before
   # our before hooks run so that we can log in (etc).
   config.prepend_before(:all, type: :system) do
-    driven_by :cuprite, screen_size: [1920, 1080], options: { headless: true, inspector: false }
+    driven_by :cuprite, screen_size: [1920, 1080], options: {
+      headless:        true,
+      inspector:       false,
+      # required for docker (github-ci)
+      browser_options: { "no-sandbox": nil },
+      # block hosts that are not required for tests
+      url_blacklist:   [
+        %r{//fonts.googleapis.com}, # inconsolata, etc
+        %r{//rsms.me}, # inter
+      ],
+    }
   end
 
   config.include Capybara::RSpecMatchers, type: :request
